85524c92d2bec4f5a9f516d56bd44fa2e513fb0b
jemu <jemu@ticonsys.com>
2023-11-21 18:29:37 +0600

    body shape measure added

--- src/renderer/src/pages/PrepforBodyMeasure.tsx
+++ src/renderer/src/pages/PrepforBodyMeasure.tsx
@@ -6,11 +6,19 @@ function PrepforBodyMeasure(): JSX.Element {
   return (
     <div className="flex flex-col w-screen justify-between">
       <TextNav {...navProperties} />
-      <div className="flex flex-col justify-center items-center w-full px-[72px]">
-        <div className="flex flex-col justify-center items-center  bg-[#FAFAFA] rounded-[20px]">
-          <p className="text-[#1A191D] text-center text-[40px] not-italic font-medium leading-[normal] tracking-[-1.2px]">
+      <div className="flex flex-col justify-center items-center px-[72px]">
+        <div className="flex flex-col justify-center items-center w-full  bg-[#FAFAFA] rounded-[20px]">
+          <p className="text-[#1A191D] text-center text-[40px] not-italic font-medium leading-[normal] tracking-[-1.2px] py-14">
             측정 안내
           </p>
+          <div className="flex flex-col justify-start items-start w-full gap-4 px-8 pb-24">
+            <p className="text-[#1A191D] text-[32px] not-italic font-normal leading-[normal] tracking-[-0.96px]">
+              1. 음성 안내에 따라 측정해주세요.
+            </p>
+            <p className="text-[#1A191D] text-[32px] not-italic font-normal leading-[normal] tracking-[-0.96px]">
+              2. 정면, 측면, 후면, 보행 4가지 측정을 진행합니다.
+            </p>
+          </div>
         </div>
       </div>
       <div className="flex flex-col gap-16 mt-10">
